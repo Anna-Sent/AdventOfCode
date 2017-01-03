f4c6d47d7d5e7064f49372c8bbe573d14f9fa465
Anna-Sentyakova <anna.sent@gmail.com>
2017-01-04 01:53:15 +0400

    test factorial

---
+++ src/test/Permutation.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Permutation {
+    public static void main(String[] args) {
+        permutate(new int[]{}, 0);
+        permutate(new int[]{1}, 0);
+        permutate(new int[]{1, 2}, 0);
+        for (int i = 3; i <= 6; ++i) {
+            int[] array = Stream.iterate(1, x -> x + 1).limit(i).mapToInt(Integer::intValue).toArray();
+            permutate(array, 0);
+        }
+    }
+
+    public static void permutate(int[] list, int pointer) {
+        if (pointer == list.length) {
+            System.out.println(String.join(",", Arrays.stream(list).mapToObj(x -> String.valueOf(x)).collect(Collectors.toList())));
+            return;
+        }
+        for (int i = pointer; i < list.length; i++) {
+            int[] permutation = list.clone();
+            permutation[pointer] = list[i];
+            permutation[i] = list[pointer];
+            permutate(permutation, pointer + 1);
+        }
+    }
+}
