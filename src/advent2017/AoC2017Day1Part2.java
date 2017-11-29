3773e45385fb9bb57aba663e6a4e0bcfe4daf096
Anna <anna.sent@gmail.com>
2017-11-29 13:42:11 +0400

    advent 2017 ready

---
+++ src/advent2017/AoC2017Day1Part2.java
@@ -0,0 +1,15 @@
+package advent2017;
+
+public class AoC2017Day1Part2 {
+    public static void main(String[] args) {
+        int result;
+
+        result = test("");
+        assert result == 0;
+        System.out.println(result);
+    }
+
+    public static int test(String s) {
+        return 0;
+    }
+}
