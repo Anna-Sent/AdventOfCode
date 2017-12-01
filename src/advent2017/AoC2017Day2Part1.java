7135e33b0857181c78496be6b0b9dc6c44c1eefa
Anna <anna.sent@gmail.com>
2017-12-01 09:27:27 +0400

    2017: day 1; next day ready

---
+++ src/advent2017/AoC2017Day2Part1.java
@@ -0,0 +1,30 @@
+package advent2017;
+
+public class AoC2017Day2Part1 {
+    public static void main(String[] args) {
+        int result;
+
+        result = test("");
+        assert result == 0;
+        System.out.println(result);
+
+        result = test("");
+        assert result == 0;
+        System.out.println(result);
+
+        result = test("");
+        assert result == 0;
+        System.out.println(result);
+
+        result = test("");
+        assert result == 0;
+        System.out.println(result);
+
+        result = test("");
+        System.out.println(result);
+    }
+
+    public static int test(String s) {
+        return 0;
+    }
+}
