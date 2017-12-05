52d055e4b183c1997545abdde4758bd5dfbb38ca
Anna <anna.sent@gmail.com>
2017-12-05 09:17:13 +0400

    2017: day 5; next day ready

---
+++ src/advent2017/AoC2017Day6Part1.java
@@ -0,0 +1,23 @@
+package advent2017;
+
+public class AoC2017Day6Part1 {
+    public static void main(String[] args) {
+        int result;
+
+        result = test("");
+        assert result == 0 : "unexpected result is " + result;
+        System.out.println(result);
+
+        result = test("");
+        //assert result ==  : "unexpected result is " + result;
+        System.out.println(result);
+    }
+
+    public static int test(String s) {
+        String[] parts = s.split("\n");
+        for (int i = 0; i < parts.length; ++i) {
+        }
+        int count = 0;
+        return count;
+    }
+}
