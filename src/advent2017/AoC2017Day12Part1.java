19161841c2896b37cbb6d285662a817b915470a2
Anna <anna.sent@gmail.com>
2017-12-11 09:54:46 +0400

    2017: day 11; next day ready

---
+++ src/advent2017/AoC2017Day12Part1.java
@@ -0,0 +1,20 @@
+package advent2017;
+
+
+public class AoC2017Day12Part1 {
+    public static void main(String[] args) {
+        int result;
+
+        result = test("");
+        System.out.println(result);
+        assert result == 0 : "unexpected result is " + result;
+
+        result = test("");
+        assert result == 0 : "unexpected result is " + result;
+        System.out.println(result);
+    }
+
+    public static int test(String s) {
+        return 0;
+    }
+}
