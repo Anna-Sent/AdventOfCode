98e077c83571819893d80b46d274456be9a261a9
Anna <anna.sent@gmail.com>
2017-12-15 09:25:53 +0400

    2017: day 15; next day ready

--- src/advent2017/AoC2017Day15Part2.java
+++ src/advent2017/AoC2017Day15Part2.java
@@ -4,16 +4,46 @@ public class AoC2017Day15Part2 {
     public static void main(String[] args) {
         int result;
 
-        result = test("");
+        result = test(65, 8921);
         System.out.println(result);
-        assert result == 0 : "unexpected result is " + result;
+        assert result == 309 : "unexpected result is " + result;
 
-        result = test("");
-        assert result == 0 : "unexpected result is " + result;
+        result = test(679, 771);
+        assert result == 306 : "unexpected result is " + result;
         System.out.println(result);
     }
 
-    public static int test(String s) {
-        return 0;
+    public static int test(long a, long b) {
+        int count = 0;
+        for (int i = 0; i < 5000000; ++i) {
+            a = generateA(a);
+            b = generateB(b);
+            if (matches(a, b)) {
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    private static boolean matches(long a, long b) {
+        return (a & 65535) == (b & 65535);
+    }
+
+    private static long generateA(long previous) {
+        for (; ; ) {
+            previous = previous * 16807 % 2147483647;
+            if (previous % 4 == 0) {
+                return previous;
+            }
+        }
+    }
+
+    private static long generateB(long previous) {
+        for (; ; ) {
+            previous = previous * 48271 % 2147483647;
+            if (previous % 8 == 0) {
+                return previous;
+            }
+        }
     }
 }
