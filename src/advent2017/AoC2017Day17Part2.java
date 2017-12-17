1f20f148c7780d3eb29225ae9713b4be53e89065
Anna <anna.sent@gmail.com>
2017-12-17 09:53:08 +0400

    2017: day 17; next days ready

--- src/advent2017/AoC2017Day17Part2.java
+++ src/advent2017/AoC2017Day17Part2.java
@@ -4,16 +4,24 @@ public class AoC2017Day17Part2 {
     public static void main(String[] args) {
         int result;
 
-        result = test("");
-        System.out.println(result);
-        assert result == 0 : "unexpected result is " + result;
-
-        result = test("");
-        assert result == 0 : "unexpected result is " + result;
+        result = test(367);
+        assert result == 25674054 : "unexpected result is " + result;
         System.out.println(result);
     }
 
-    public static int test(String s) {
-        return 0;
+    public static int test(int steps) {
+        int count = 0;
+        int index = 0;
+        int result = 0;
+        while (count <= 50000000) {
+            index += steps;
+            index = count == 0 ? 0 : index % count;
+            index += count == 0 ? 0 : 1;
+            if (index == 1) {
+                result = count;
+            }
+            ++count;
+        }
+        return result;
     }
 }
---
+++ src/advent2017/AoC2017Day23Part2.java
@@ -0,0 +1,19 @@
+package advent2017;
+
+public class AoC2017Day23Part2 {
+    public static void main(String[] args) {
+        int result;
+
+        result = test("");
+        System.out.println(result);
+        assert result == 0 : "unexpected result is " + result;
+
+        result = test("");
+        //assert result ==  : "unexpected result is " + result;
+        System.out.println(result);
+    }
+
+    public static int test(String s) {
+        return 0;
+    }
+}
