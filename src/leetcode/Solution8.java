ad2c70975c3b87e4465019bd965e5e89c6eaef2b
Anna Sentyakova <anna.sent@gmail.com>
2018-04-08 20:50:25 +0400

    leetcode solutions

---
+++ src/leetcode/Solution8.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int myAtoi(String str) {
+        str = str.trim();
+        if (str.length() == 0)
+            return 0;
+        char ch = str.charAt(0);
+        int sign = 1, i = 0;
+        if (ch == '+') {
+            i = 1;
+        } else if (ch == '-') {
+            sign = -1;
+            i = 1;
+        }
+        long result = 0;
+        while (i < str.length()) {
+            ch = str.charAt(i);
+            if (ch < '0' || ch > '9') {
+                break;
+            }
+            result = result * 10 + (ch - '0');
+            if (sign * result > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            } else if (sign * result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            ++i;
+        }
+        return sign * (int) result;
+    }
+}
---
+++ src/leetcode/Solution9.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        long palindrome = 0;
+        int xCopy = x;
+        while (x > 0) {
+            palindrome = palindrome * 10 + (x % 10);
+            x /= 10;
+        }
+        return palindrome == xCopy;
+    }
+}
