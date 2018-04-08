ffa2ac9e6a7fd656720a9dd80e0f26509aba9eb9
Anna Sentyakova <anna.sent@gmail.com>
2018-04-08 23:53:56 +0400

    leetcode solutions

---
+++ src/leetcode/Solution234.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode tail = head;
+        int count = 1;
+        while (tail.next != null) {
+            tail = tail.next;
+            ++count;
+        }
+        int half = count >> 1;
+        ListNode secondHead = head;
+        int i = 1;
+        while (i <= half) {
+            secondHead = secondHead.next;
+            ++i;
+        }
+        if ((count & 1) == 1) {
+            secondHead = secondHead.next;
+        }
+        ListNode prev = null, tmp;
+        while (secondHead != null) {
+            tmp = secondHead.next;
+            secondHead.next = prev;
+            prev = secondHead;
+            secondHead = tmp;
+        }
+        secondHead = tail;
+        while (secondHead != null && head != null) {
+            if (secondHead.val != head.val) {
+                return false;
+            }
+            secondHead = secondHead.next;
+            head = head.next;
+        }
+        return true;
+    }
+}
