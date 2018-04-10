ae43ab3e1585ef70e08bc98cfa781a17ac802492
Anna Sentyakova <anna.sent@gmail.com>
2018-04-10 12:39:27 +0400

    leetcode solutions

---
+++ src/leetcode/Solution136.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class Solution136 {
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int missing = 0;
+            for (int i = 0; i < nums.length; ++i) {
+                missing ^= nums[i];
+            }
+            return missing;
+        }
+    }
+}
