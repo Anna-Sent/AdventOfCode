950d1a888a8338e6ac70694794c9539b14728805
Anna Sentyakova <anna.sent@gmail.com>
2018-04-11 16:40:16 +0400

    leetcode solutions

---
+++ src/leetcode/Solution35.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+import java.util.Arrays;
+
+class Solution35 {
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+            int index = Arrays.binarySearch(nums, target);
+            return index >= 0 ? index : -index - 1;
+        }
+    }
+}
