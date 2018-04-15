62ed0686ac950cc0e61b305078808d37b2f82b85
Anna <anna.sent@gmail.com>
2018-04-15 23:12:03 +0400

    leetcode solutions

---
+++ src/leetcode/Solution628.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Solution628 {
+    class Solution {
+        public int maximumProduct(int[] nums) {
+            Arrays.sort(nums);
+            int len = nums.length;
+            int product1 = nums[len - 3] * nums[len - 2] * nums[len - 1];
+            int product2 = nums[0] * nums[1] * nums[len - 1];
+            return Math.max(product1, product2);
+        }
+    }
+}
