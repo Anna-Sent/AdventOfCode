f6a01b9b3e28e7906930b534f656f935f5a51348
Anna Sentyakova <anna.sent@gmail.com>
2018-05-11 17:13:57 +0400

    leetcode solutions

---
+++ src/leetcode/Solution476.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class Solution476 {
+    class Solution {
+        public int findComplement(int num) {
+            if (num < 0) {
+                return 0;
+            }
+            if (num == 0) {
+                return 1;
+            }
+            int result = 0;
+            int p = 1;
+            while (num > 0) {
+                result += (1 - (num & 1)) * p;
+                num >>>= 1;
+                p <<= 1;
+            }
+            return result;
+        }
+    }
+}
