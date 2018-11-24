078f07e785aa0d9d2aef220b765ccf1eb6890b5d
Anna <anna.sent@gmail.com>
2018-11-24 20:30:37 +0400

    ready to AoC 2018

---
+++ src/advent2018/AoC2018Day16Part1.java
@@ -0,0 +1,20 @@
+package advent2018;
+
+public class AoC2018Day16Part1 {
+    public static void main(String[] args) {
+        int result;
+
+        result = test("1122");
+        assert result == 3 : "unexpected result is" + result;
+        System.out.println(result);
+
+        result = test("1111");
+        assert result == 4 : "unexpected result is" + result;
+        System.out.println(result);
+    }
+
+    public static int test(String s) {
+        int sum = 0;
+        return sum;
+    }
+}
