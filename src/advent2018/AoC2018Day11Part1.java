c21ca2c4559599f546e0b81327c544362e92490c
Anna Sentyakova <anna.sent@gmail.com>
2018-12-11 09:40:16 +0400

    day 11: 1059 / 614 -- brute force

--- src/advent2018/AoC2018Day11Part1.java
+++ src/advent2018/AoC2018Day11Part1.java
@@ -1,20 +1,77 @@
 package advent2018;
 
 public class AoC2018Day11Part1 {
-    public static void main(String[] args) {
+    private static void testPowerLevel() {
         int result;
 
-        result = test("1122");
-        assert result == 3 : "unexpected result is " + result;
+        result = powerLevel(3, 5, 8);
+        assert result == 4 : "unexpected result is " + result;
         System.out.println(result);
 
-        result = test("1111");
+        result = powerLevel(122, 79, 57);
+        assert result == -5 : "unexpected result is " + result;
+        System.out.println(result);
+
+        result = powerLevel(217, 196, 39);
+        assert result == 0 : "unexpected result is " + result;
+        System.out.println(result);
+
+        result = powerLevel(101, 153, 71);
         assert result == 4 : "unexpected result is " + result;
         System.out.println(result);
     }
 
-    public static int test(String s) {
-        int sum = 0;
-        return sum;
+    public static void main(String[] args) {
+        testPowerLevel();
+
+        String result;
+
+        result = findLargest(18);
+        assert result.equals("29 33,45") : "unexpected result is " + result;
+        System.out.println(result);
+
+        result = findLargest(42);
+        assert result.equals("30 21,61") : "unexpected result is " + result;
+        System.out.println(result);
+
+        result = findLargest(6392);
+        assert result.equals("30 20,58") : "unexpected result is " + result;
+        System.out.println(result);
+    }
+
+    private static int powerLevel(int x, int y, int serialNumber) {
+        int rackId = x + 10;
+        int result = rackId * y;
+        result += serialNumber;
+        result *= rackId;
+        result %= 1000;
+        result /= 100;
+        result -= 5;
+        return result;
+    }
+
+    public static String findLargest(int serialNumber) {
+        Result maxResult = null;
+        for (int y = 1; y <= 300 - 2; ++y) {
+            for (int x = 1; x <= 300 - 2; ++x) {
+                Result result = new Result();
+                result.x = x;
+                result.y = y;
+                result.totalPowerLevel = 0;
+                for (int dy = y; dy < y + 3; ++dy) {
+                    for (int dx = x; dx < x + 3; ++dx) {
+                        result.totalPowerLevel += powerLevel(dx, dy, serialNumber);
+                    }
+                }
+                if (maxResult == null || maxResult.totalPowerLevel < result.totalPowerLevel) {
+                    maxResult = result;
+                }
+            }
+        }
+        return maxResult.totalPowerLevel + " " + maxResult.x + "," + maxResult.y;
+    }
+
+    private static class Result {
+        int x, y, totalPowerLevel;
     }
 }
--- src/advent2018/AoC2018Day11Part2.java
+++ src/advent2018/AoC2018Day11Part2.java
@@ -1,20 +1,80 @@
 package advent2018;
 
 public class AoC2018Day11Part2 {
-    public static void main(String[] args) {
+    private static void testPowerLevel() {
         int result;
 
-        result = test("1122");
-        assert result == 3 : "unexpected result is " + result;
+        result = powerLevel(3, 5, 8);
+        assert result == 4 : "unexpected result is " + result;
         System.out.println(result);
 
-        result = test("1111");
+        result = powerLevel(122, 79, 57);
+        assert result == -5 : "unexpected result is " + result;
+        System.out.println(result);
+
+        result = powerLevel(217, 196, 39);
+        assert result == 0 : "unexpected result is " + result;
+        System.out.println(result);
+
+        result = powerLevel(101, 153, 71);
         assert result == 4 : "unexpected result is " + result;
         System.out.println(result);
     }
 
-    public static int test(String s) {
-        int sum = 0;
-        return sum;
+    public static void main(String[] args) {
+        testPowerLevel();
+
+        String result;
+
+        result = findLargest(18);
+        assert result.equals("113 90,269,16") : "unexpected result is " + result;
+        System.out.println(result);
+
+        result = findLargest(42);
+        assert result.equals("119 232,251,12") : "unexpected result is " + result;
+        System.out.println(result);
+
+        result = findLargest(6392);
+        assert result.equals("83 233,268,13") : "unexpected result is " + result;
+        System.out.println(result);
+    }
+
+    private static int powerLevel(int x, int y, int serialNumber) {
+        int rackId = x + 10;
+        int result = rackId * y;
+        result += serialNumber;
+        result *= rackId;
+        result %= 1000;
+        result /= 100;
+        result -= 5;
+        return result;
+    }
+
+    public static String findLargest(int serialNumber) {
+        Result maxResult = null;
+        for (int size = 1; size <= 300; ++size) {
+            for (int y = 1; y <= 300 - size; ++y) {
+                for (int x = 1; x <= 300 - size; ++x) {
+                    Result result = new Result();
+                    result.x = x;
+                    result.y = y;
+                    result.size = size;
+                    result.totalPowerLevel = 0;
+                    for (int dy = y; dy < y + size; ++dy) {
+                        for (int dx = x; dx < x + size; ++dx) {
+                            result.totalPowerLevel += powerLevel(dx, dy, serialNumber);
+                        }
+                    }
+                    if (maxResult == null || maxResult.totalPowerLevel < result.totalPowerLevel) {
+                        maxResult = result;
+                    }
+                }
+            }
+        }
+        return maxResult.totalPowerLevel + " " + maxResult.x + "," + maxResult.y + "," + maxResult.size;
+    }
+
+    private static class Result {
+        int x, y, size, totalPowerLevel;
     }
 }
