0e50e4c5850789457e9735a0d10ab8dbbc9950da
Anna Sentyakova <anna.sent@gmail.com>
2018-12-19 14:34:33 +0400

    day 19: added comment

--- src/advent2018/AoC2018Day19Part2Alternate.java
+++ src/advent2018/AoC2018Day19Part2Alternate.java
@@ -2,6 +2,11 @@ package advent2018;
 
 public class AoC2018Day19Part2Alternate {
     public static void main(String[] args) {
+        // r0 -- result
+        // r1, r3 -- counters
+        // r2 -- instruction pointer
+        // r4 -- input number
+        // r5 -- multiplication of r1 and r3
         System.out.println(calculateSumOfDividers(10551275));
     }
 
