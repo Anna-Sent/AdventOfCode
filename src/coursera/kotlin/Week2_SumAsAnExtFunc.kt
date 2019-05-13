d23232f8c5de1f5fddaf8d2c590be67335136729
Anna Sentyakova <anna.sent@gmail.com>
2019-05-13 16:48:35 +0400

    coursera kotlin week 2 sum as an extension function

---
+++ src/coursera/kotlin/Week2_SumAsAnExtFunc.kt
@@ -0,0 +1,14 @@
+package coursera.kotlin
+
+fun List<Int>.sum(): Int {
+    var result = 0
+    for (i in this) {
+        result += i
+    }
+    return result
+}
+
+fun main() {
+    val sum = listOf(1, 2, 3).sum()
+    println(sum)    // 6
+}
