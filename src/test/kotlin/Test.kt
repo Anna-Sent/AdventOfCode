1645749c0708f7ee679e25307d1b684f1945685c
Anna Sentyakova <anna.sent@gmail.com>
2019-05-13 19:10:11 +0400

    test kotlin

---
+++ src/test/kotlin/Test.kt
@@ -0,0 +1,15 @@
+package test.kotlin
+
+import java.awt.Color
+
+fun main() {
+    System.out.println(value(Color.RED))
+}
+
+fun value(color: Color): String {
+    return when (color) {
+        Color.BLACK -> "black"
+        Color.RED -> "red"
+        else -> "else"
+    }
+}
