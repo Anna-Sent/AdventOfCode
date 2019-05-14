fa273d6071740c67e8e1307af6cbad62975438ea
Anna Sentyakova <anna.sent@gmail.com>
2019-05-14 14:49:32 +0400

    coursera kotlin week 3 isEmptyOrNull

---
+++ src/coursera/kotlin/Week3_isEmptyOrNull.kt
@@ -0,0 +1,17 @@
+package coursera.kotlin
+
+infix fun Boolean.eq(param: Boolean) {
+    System.out.println(if (this == param) "OK" else "FAILED")
+}
+
+fun String?.isEmptyOrNull() = this == null || this.isEmpty()
+
+fun main() {
+    val s1: String? = null
+    val s2: String? = ""
+    s1.isEmptyOrNull() eq true
+    s2.isEmptyOrNull() eq true
+
+    val s3 = "   "
+    s3.isEmptyOrNull() eq false
+}
