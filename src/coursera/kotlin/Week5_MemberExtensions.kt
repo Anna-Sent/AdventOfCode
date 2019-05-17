f3b1a644f36afc95c8b407e9190901023c276b7d
Anna Sentyakova <anna.sent@gmail.com>
2019-05-17 15:38:09 +0400

    coursera kotlin week 5 member extensions

---
+++ src/coursera/kotlin/Week5_MemberExtensions.kt
@@ -0,0 +1,24 @@
+package coursera.kotlin
+
+class Words {
+    private val list = mutableListOf<String>()
+
+    fun String.record(): Words {
+        list += this
+        return this@Words
+    }
+
+    operator fun Words.plus(s: String) {
+        list += s
+    }
+
+    override fun toString() = list.toString()
+}
+
+fun main() {
+    val words = Words()
+    with(words) {
+        "one".record() + "two"
+    }
+    words.toString() eq "[one, two]"
+}
