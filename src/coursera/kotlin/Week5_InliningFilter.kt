ba8f0af39e1ba0bb43e8198f521a426eb1af9d98
Anna Sentyakova <anna.sent@gmail.com>
2019-05-17 12:45:05 +0400

    coursera kotlin week 5 inlining filter

---
+++ src/coursera/kotlin/Week5_InliningFilter.kt
@@ -0,0 +1,34 @@
+package coursera.kotlin
+
+infix fun String.eq(param: String) {
+    System.out.println(if (this == param) "OK" else "FAILED")
+}
+
+fun filterNonZero(list: List<Int>) = list.filter { it != 0 }
+
+fun filterNonZeroGenerated(list: List<Int>): List<Int> {
+    val destination = ArrayList<Int>()
+    for (element in list) {
+        if (element != 0) {
+            destination.add(element)
+        }
+    }
+    return destination
+}
+
+fun main() {
+    val list = listOf(1, 2, 3, 0)
+
+    filterNonZero(list).toString() eq "[1, 2, 3]"
+    filterNonZeroGenerated(list).toString() eq "[1, 2, 3]"
+}
+
+inline fun <T> Iterable<T>.filter(predicate: (T) -> Boolean): List<T> {
+    val destination = ArrayList<T>()
+    for (element in this) {
+        if (predicate(element)) {
+            destination.add(element)
+        }
+    }
+    return destination
+}
