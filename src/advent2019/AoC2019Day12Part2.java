a6b44e478b05e87aa3b9117f064811ea8b6a4485
Anna Sentyakova <anna.sent@gmail.com>
2019-12-01 00:56:35 +0400

    ready to AoC 2019

---
+++ src/advent2019/AoC2019Day12Part2.java
@@ -0,0 +1,21 @@
+package advent2019;
+
+public class AoC2019Day12Part2 {
+
+    public static void main(String[] args) {
+        int result;
+
+        result = test("1122");
+        assert result == 3 : "unexpected result is" + result;
+        System.out.println(result);
+
+        result = test("1111");
+        assert result == 4 : "unexpected result is" + result;
+        System.out.println(result);
+    }
+
+    public static int test(String s) {
+        int sum = 0;
+        return sum;
+    }
+}
