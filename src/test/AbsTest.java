5100738d0439775bb39699353c0522f9d4069b52
Anna Sentyakova <anna.sent@gmail.com>
2020-02-04 18:13:28 +0400

    test abs

---
+++ src/test/AbsTest.java
@@ -0,0 +1,9 @@
+package test;
+
+public class AbsTest {
+
+    public static void main(String[] args) {
+        System.out.println(Math.abs(Integer.MIN_VALUE + 1));
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+    }
+}
