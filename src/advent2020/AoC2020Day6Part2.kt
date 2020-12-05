ff0a764e53ab5f672e51b59207a4d02cc3aca389
Anna Sentyakova <anna.sent@gmail.com>
2020-12-05 17:54:38 +0400

    day 6: preparing

---
+++ src/advent2020/AoC2020Day6Part2.kt
@@ -0,0 +1,22 @@
+package advent2020
+
+private fun test(input: String): Int {
+    val entries = input.split("\n")
+    return 0
+}
+
+fun main() {
+    test()
+}
+
+private var result: Int = 0
+
+private fun test() {
+    result = test("""
+    """.trimIndent())
+    check(0, result)
+
+    result = test("""
+    """.trimIndent())
+    check(0, result)
+}
