aad85e66bff018453a10f4db99d1beecb313df76
Anna Sentyakova <anna.sent@gmail.com>
2020-12-13 10:23:27 +0400

    day 13, part 1

--- src/advent2020/AoC2020Day13Part1.kt
+++ src/advent2020/AoC2020Day13Part1.kt
@@ -1,8 +1,17 @@
 package advent2020
 
-private fun test(input: String): Int {
-    val numbers = input.split("\n").map { it.toInt() }
-    return 0
+private fun test(start: Int, input: String): Int {
+    val ids = input.split(",").mapNotNull { it.toIntOrNull() }
+    var min: Int? = null
+    var minId: Int? = null
+    for (id in ids) {
+        val minutesToNext = id - start % id
+        if (min == null || min > minutesToNext) {
+            min = minutesToNext
+            minId = id
+        }
+    }
+    return minId!! * min!!
 }
 
 fun main() {
@@ -12,15 +21,15 @@ fun main() {
 private var result: Int = 0
 
 private fun test() {
-    result = test("""
-    """.trimIndent())
-    check(0, result)
+    result = test(
+            939,
+            "7,13,x,x,59,x,31,19"
+    )
+    check(295, result)
 
-    result = test("""
-    """.trimIndent())
-    check(0, result)
-
-    result = test("""
-    """.trimIndent())
-    check(0, result)
+    result = test(
+            1014511,
+            "17,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,643,x,x,x,x,x,x,x,23,x,x,x,x,13,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,29,x,433,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,19"
+    )
+    check(3464, result)
 }
