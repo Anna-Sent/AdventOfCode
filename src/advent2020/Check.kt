c38aa7954713549f511816e10a86421af601bc64
Anna Sentyakova <anna.sent@gmail.com>
2020-12-21 18:41:28 +0400

    day 21: 8182 / 7932

--- src/advent2020/Check.kt
+++ src/advent2020/Check.kt
@@ -23,3 +23,15 @@ fun check(expected: Long, actual: Long, fall: Boolean = true) {
         }
     }
 }
+
+fun check(expected: String, actual: String, fall: Boolean = true) {
+    println(actual)
+    if (expected != actual) {
+        val msg = "expected $expected, but was: $actual"
+        if (fall) {
+            assert(false) { msg }
+        } else {
+            println(msg)
+        }
+    }
+}
