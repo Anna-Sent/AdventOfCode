304f0d755f294622797cc7568548f4fc6c16cda7
Anna Sentyakova <anna.sent@gmail.com>
2020-12-23 10:36:20 +0400

    day 23: 3369 / 3402

--- src/advent2020/AoC2020Day23Part1.kt
+++ src/advent2020/AoC2020Day23Part1.kt
@@ -1,26 +1,88 @@
 package advent2020
 
-private fun test(input: String): Int {
-    val numbers = input.split("\n").map { it.toInt() }
-    return 0
+private fun test(input: String, count: Int): String {
+    var numbers = input.map { it.toString().toInt() }.toIntArray()
+
+    val min = numbers.min()!!
+    val max = numbers.max()!!
+    val size = numbers.size
+    val current = 0
+    for (i in 1..count) {
+
+        println("move $i")
+        println("cups: ${numbers.joinToString(" ")}")
+        println("current: ${numbers[current]}")
+
+        val pickedUpIndicies = intArrayOf((current + 1) % size, (current + 2) % size, (current + 3) % size)
+        val pickedUpValues = pickedUpIndicies.map { numbers[it] }.toIntArray()
+
+        println("pick up: ${pickedUpValues.joinToString(" ")}")
+
+        var find = numbers[current] - 1
+        if (find < min) {
+            find = max
+        }
+        var destination = numbers.find { it == find }!!
+        while (pickedUpValues.contains(destination)) {
+            --destination
+            if (destination < min) {
+                destination = max
+            }
+        }
+
+        println("destination: $destination")
+        println()
+
+        val list = mutableListOf<Int>()
+        var added = false
+        for (number in numbers) {
+            if (number !in pickedUpValues) {
+                list += number
+            }
+            if (number == destination) {
+                added = true
+                list += pickedUpValues.toList()
+            }
+        }
+        if (!added) {
+            list += pickedUpValues.toList()
+        }
+
+        val head = list[0]
+        list.removeAt(0)
+        list += head
+
+        numbers = list.toIntArray()
+    }
+
+    val result = mutableListOf<Int>()
+    var i = 0
+    var startAdding = false
+    while (result.size != numbers.size - 1) {
+        if (numbers[i] == 1) {
+            startAdding = true
+        } else if (startAdding) {
+            result += numbers[i]
+        }
+        i = (i + 1) % numbers.size
+    }
+
+    return result.joinToString("")
 }
 
 fun main() {
     test()
 }
 
-private var result: Int = 0
+private var result: String = ""
 
 private fun test() {
-    result = test("""
-    """.trimIndent())
-    check(0, result)
+    result = test("389125467", 10)
+    check("92658374", result)
 
-    result = test("""
-    """.trimIndent())
-    check(0, result)
+    result = test("389125467", 100)
+    check("67384529", result)
 
-    result = test("""
-    """.trimIndent())
-    check(0, result)
+    result = test("123487596", 100)
+    check("47598263", result)
 }
--- src/advent2020/AoC2020Day23Part2.kt
+++ src/advent2020/AoC2020Day23Part2.kt
@@ -1,26 +1,86 @@
 package advent2020
 
-private fun test(input: String): Int {
-    val numbers = input.split("\n").map { it.toInt() }
-    return 0
+private fun test(input: String, count: Int, add: Int): Long {
+    val numbers = input.map { it.toString().toInt() }.toMutableList()
+
+    val min = numbers.min()!!
+    var max = numbers.max()!!
+
+    for (i in max + 1..add) {
+        numbers.add(i)
+    }
+
+    max = add.coerceAtLeast(max)
+
+    val rootNode = Node2(numbers[0], null)
+    var currentNode = rootNode
+    val nodes = mutableMapOf<Int, Node2>()
+    nodes[rootNode.value] = rootNode
+    for (i in 1 until numbers.size) {
+        val node = Node2(numbers[i], null)
+        nodes[node.value] = node
+        currentNode.next = node
+        currentNode = node
+    }
+    currentNode.next = rootNode
+
+    currentNode = rootNode
+    for (i in 1..count) {
+
+//        println("move $i")
+//        println("current: ${currentNode.value}")
+
+        val pickedUpValues = listOf(currentNode.next!!, currentNode.next!!.next!!, currentNode.next!!.next!!.next!!)
+
+//        println("pick up: ${pickedUpValues.map { it.value }.toList()}")
+
+        var find = currentNode.value - 1
+        if (find < min) {
+            find = max
+        }
+        var destinationNode = nodes[find]!!
+        while (pickedUpValues.contains(destinationNode)) {
+            --find
+            if (find < min) {
+                find = max
+            }
+            destinationNode = nodes[find]!!
+        }
+
+//        println("destination: ${destinationNode.value}")
+//        println()
+
+        currentNode.next = currentNode.next!!.next!!.next!!.next
+
+        val tmp = destinationNode.next
+        destinationNode.next = pickedUpValues[0]
+        pickedUpValues[2].next = tmp
+
+        currentNode = currentNode.next!!
+    }
+
+    val node1 = nodes[1]!!
+    return node1.next!!.value.toLong() * node1.next!!.next!!.value.toLong()
 }
+
+private class Node2(val value: Int, var next: Node2?)
 
 fun main() {
     test()
 }
 
-private var result: Int = 0
+private var result: Long = 0
 
 private fun test() {
-    result = test("""
-    """.trimIndent())
-    check(0, result)
+    result = test("389125467", 10, 0)
+    check(18, result)
 
-    result = test("""
-    """.trimIndent())
-    check(0, result)
+    result = test("389125467", 100, 0)
+    check(42, result)
 
-    result = test("""
-    """.trimIndent())
-    check(0, result)
+    result = test("389125467", 10_000_000, 1_000_000)
+    check(149245887792, result)
+
+    result = test("123487596", 10_000_000, 1_000_000)
+    check(248009574232, result)
 }
