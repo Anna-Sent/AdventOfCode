28caeef7173c47fa9b03e3fbcc8a790019ed7afd
Anna Sentyakova <anna.sent@gmail.com>
2020-12-25 10:07:29 +0400

    day 25: 3209 / 2588

--- src/advent2020/AoC2020Day25.kt
+++ src/advent2020/AoC2020Day25.kt
@@ -1,26 +1,43 @@
 package advent2020
 
-private fun test(input: String): Int {
-    val numbers = input.split("\n").map { it.toInt() }
-    return 0
+private fun test(pk1: Long, pk2: Long): Long {
+    val d = findLoopSize(pk2)
+    return calcPk(pk1, d)
+}
+
+private const val GENERATOR = 20201227L
+
+private fun findLoopSize(pk: Long): Long {
+    var probablePk = 1L
+    for (i in 1..GENERATOR) {
+        probablePk *= 7
+        probablePk %= GENERATOR
+        if (probablePk == pk) {
+            return i
+        }
+    }
+    return -1
+}
+
+private fun calcPk(subject: Long, loopSize: Long): Long {
+    var pk = 1L
+    for (i in 1..loopSize) {
+        pk *= subject
+        pk %= GENERATOR
+    }
+    return pk
 }
 
 fun main() {
     test()
 }
 
-private var result: Int = 0
+private var result: Long = 0
 
 private fun test() {
-    result = test("""
-    """.trimIndent())
-    check(0, result)
+    result = test(5764801, 17807724)
+    check(14897079, result)
 
-    result = test("""
-    """.trimIndent())
-    check(0, result)
-
-    result = test("""
-    """.trimIndent())
-    check(0, result)
+    result = test(14788856, 19316454)
+    check(545789, result)
 }
