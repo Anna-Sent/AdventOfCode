f0785b714732d53fc2c17b531b5e245558f60708
Anna Sentyakova <anna.sent@gmail.com>
2020-12-31 00:28:40 +0400

    solution 3

--- src/leetcode/solution0/Solution3.kt
+++ src/leetcode/solution0/Solution3.kt
@@ -3,7 +3,7 @@ package leetcode.solution0
 import leetcode.eq
 
 class Solution3 {
-    class Solution {
+    class Solution1 {
         fun lengthOfLongestSubstring(s: String): Int {
             var max = 0
             var i = 0
@@ -24,12 +24,53 @@ class Solution3 {
             return max
         }
     }
+
+    class Solution2 {
+        fun lengthOfLongestSubstring(s: String): Int {
+            var max = 0
+            var k = 0
+            val map = mutableMapOf<Char, Int>()
+            for ((i, ch) in s.withIndex()) {
+                val value = map[ch]
+                if (value != null) {
+                    if (i - k > value) {
+                        ++k
+                    } else {
+                        k = i - value
+                    }
+                } else {
+                    ++k
+                }
+                max = max.coerceAtLeast(k)
+                map[ch] = i
+            }
+            return max
+        }
+    }
 }
 
+private val cases = mapOf(
+        "abcabcbb" to 3,
+        "bbbbb" to 1,
+        "pwwkew" to 3,
+        "" to 0,
+        " " to 1,
+        "  " to 1,
+        "ab" to 2,
+        "abc" to 3,
+        "aab" to 2,
+        "cdd" to 2
+)
+
 fun main() {
-    Solution3.Solution().lengthOfLongestSubstring("abcabcbb") eq 3
-    Solution3.Solution().lengthOfLongestSubstring("bbbbb") eq 1
-    Solution3.Solution().lengthOfLongestSubstring("pwwkew") eq 3
-    Solution3.Solution().lengthOfLongestSubstring("") eq 0
-    Solution3.Solution().lengthOfLongestSubstring(" ") eq 1
+    println("solution 1=========")
+    for ((input, expected) in cases) {
+        print("\"$input\" => ")
+        Solution3.Solution1().lengthOfLongestSubstring(input) eq expected
+    }
+    println("solution 2=========")
+    for ((input, expected) in cases) {
+        print("\"$input\" => ")
+        Solution3.Solution2().lengthOfLongestSubstring(input) eq expected
+    }
 }
