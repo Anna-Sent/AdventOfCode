bf68a8196e490ea5bae4a39e8bb01d1623d46c48
Anna Sentyakova <anna.sent@gmail.com>
2021-01-11 19:41:49 +0400

    fixing delays

--- src/test/ExpTest.kt
+++ src/test/ExpTest.kt
@@ -3,20 +3,22 @@ package test
 import java.util.concurrent.TimeUnit
 import kotlin.math.min
 import kotlin.math.pow
+import kotlin.random.Random
 
 fun main() {
-    test(1_000, 30_000, 55)
-    //test(100, 2_000, 55)
+    test(1_000, 30_000, 1000)
+    test(100, 2_000, 1000)
 }
 
 private fun test(baseDelay: Long, maxDelay: Long, max: Int) {
     var sum = 0L
     for (i in 0..max) {
-        val delay = exponentialDelay(i, 1_000, 30_000)
+        val delay = exponentialDelay(i, baseDelay, maxDelay)
         if (delay > 0) sum += delay
-        println("$i => $delay (${TimeUnit.MILLISECONDS.toMinutes(sum)})")
+        println("($baseDelay $maxDelay) $i => $delay (${TimeUnit.MILLISECONDS.toMinutes(sum)})")
     }
 }
 
 private fun exponentialDelay(tryCount: Int, baseDelay: Long, maxDelay: Long): Long =
-        min(maxDelay, 2.0.pow(tryCount).toLong() * baseDelay) * (1 + (0..1).random()) / 2
+        (min(maxDelay.toDouble(), 2.0.pow(tryCount) * baseDelay) * (1 + Random.nextDouble()) / 2)
+                .toLong()
