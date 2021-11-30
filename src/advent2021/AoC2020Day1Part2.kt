89cb834a88ec0779ec9a71b9a0b15b27de0e438b
Anna Sentyakova <anna.sent@gmail.com>
2021-12-01 00:42:44 +0400

    preparing for advent 2021

---
+++ src/advent2021/AoC2020Day1Part2.kt
@@ -0,0 +1,17 @@
+package advent2021
+
+private var result = 0
+
+fun main() {
+    result = test("""
+    """.trimIndent())
+    check(0, result)
+
+    result = test("""
+    """.trimIndent())
+    check(0, result)
+}
+
+private fun test(input: String): Int {
+    return 0
+}
