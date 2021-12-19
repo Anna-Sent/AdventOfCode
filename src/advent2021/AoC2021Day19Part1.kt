649467f4317386f8c9118b34c3e4bc8a5d4fda19
Anna Sentyakova <anna.sent@gmail.com>
2021-12-19 09:12:22 +0400

    day 19: 2192 / 2026

--- src/advent2021/AoC2021Day19Part1.kt
+++ src/advent2021/AoC2021Day19Part1.kt
@@ -1,23 +1,32 @@
 package advent2021
 
+import utils.Point3D
+
 private var result = 0
 
 fun main() {
-    result = test(
-        """
-    """.trimIndent()
-    )
-    check(0, result)
+    result = test(INPUT_1)
+    check(79, result)
 
-    result = test(
-        """
-    """.trimIndent()
-    )
-    check(0, result)
+    result = test(INPUT_2)
+    check(318, result)
+
+    result = test(INPUT_3)
+    check(447, result)
+}
+
+private fun Scanner.add(other: Scanner, relPoint: Point3D) {
+    for (beacon in other.beacons) {
+        val point = beacon - relPoint
+        if (point !in beacons) beacons += point
+    }
 }
 
 private fun test(input: String): Int {
-    val commands = input.split("\n")
-    var count = 0
-    return count
+    val (positions, scanners) = findPositions(input)
+    val result = Scanner(scanners[0])
+    for (i in 1 until positions.size) {
+        result.add(scanners[i], positions[i])
+    }
+    return result.beacons.size
 }
