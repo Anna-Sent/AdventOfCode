7b3b2a978dd4700eb6bbf0103100566f159c1821
Anna Sentyakova <annasentyakova@Annas-MacBook-Pro.local>
2022-12-01 02:42:57 +0300

    preparing

---
+++ src/advent2022/AoC2022Day1Part1.kt
@@ -0,0 +1,14 @@
+package advent2022
+
+private var result = 0
+
+fun main() {
+    result = test("""
+    """.trimIndent())
+    check(0, result)
+}
+
+private fun test(input: String): Int {
+    println("test test test test test test")
+    return 0
+}
---
+++ src/advent2022/Check.kt
@@ -0,0 +1,37 @@
+package advent2022
+
+fun check(expected: Int, actual: Int, fall: Boolean = true) {
+    println(actual)
+    if (expected != actual) {
+        val msg = "expected $expected, but was: $actual"
+        if (fall) {
+            assert(false) { msg }
+        } else {
+            println(msg)
+        }
+    }
+}
+
+fun check(expected: Long, actual: Long, fall: Boolean = true) {
+    println(actual)
+    if (expected != actual) {
+        val msg = "expected $expected, but was: $actual"
+        if (fall) {
+            assert(false) { msg }
+        } else {
+            println(msg)
+        }
+    }
+}
+
+fun check(expected: String, actual: String, fall: Boolean = true) {
+    println(actual)
+    if (expected != actual) {
+        val msg = "expected $expected, but was: $actual"
+        if (fall) {
+            assert(false) { msg }
+        } else {
+            println(msg)
+        }
+    }
+}
