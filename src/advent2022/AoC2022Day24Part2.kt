be427d798204de922a9c18f8ea33dd4ed47cb1c6
Anna Sentyakova <anna.sent@gmail.com>
2022-12-03 13:31:18 +0300

    preparing

---
+++ src/advent2022/AoC2022Day24Part2.kt
@@ -0,0 +1,13 @@
+package advent2022
+
+private var result = 0
+
+fun main() {
+    result = test("""
+    """.trimIndent())
+    check(0, result)
+}
+
+private fun test(input: String): Int {
+    return 0
+}
