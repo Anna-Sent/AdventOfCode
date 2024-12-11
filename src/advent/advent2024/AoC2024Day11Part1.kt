b504c3eda7a9504123cb829656b781a594e9b211
Anna Sentiakova <anna.sent@gmail.com>
2024-12-11 10:27:33 +0300

    day 11: 2533 / 8506

--- src/advent/advent2024/AoC2024Day11Part1.kt
+++ src/advent/advent2024/AoC2024Day11Part1.kt
@@ -5,17 +5,43 @@ private var result = 0
 fun main() {
     result = test(
         """
-    """.trimIndent()
+            125 17
+    """.trimIndent(), 6
     )
-    check(0, result)
+    check(22, result)
 
     result = test(
         """
-    """.trimIndent()
+            125 17
+    """.trimIndent(), 25
     )
-    check(0, result)
+    check(55312, result)
+
+    result = test(
+        """
+            9759 0 256219 60 1175776 113 6 92833
+    """.trimIndent(), 25
+    )
+    check(186996, result)
 }
 
-private fun test(input: String): Int {
-    return 0
+private fun test(input: String, count: Int): Int {
+    var numbers = input.split(" ").map(String::toLong)
+    for (i in 1..count) {
+        val newNumbers = mutableListOf<Long>()
+        for (number in numbers) {
+            val numberStr = number.toString()
+            when {
+                number == 0L -> newNumbers += 1
+                numberStr.length % 2 == 0 -> {
+                    newNumbers += numberStr.substring(0, numberStr.length / 2).toLong()
+                    newNumbers += numberStr.substring(numberStr.length / 2).toLong()
+                }
+
+                else -> newNumbers += number * 2024
+            }
+        }
+        numbers = newNumbers
+    }
+    return numbers.size
 }
--- src/advent/advent2024/AoC2024Day11Part2.kt
+++ src/advent/advent2024/AoC2024Day11Part2.kt
@@ -1,21 +1,62 @@
 package advent.advent2024
 
-private var result = 0
+private var result = 0L
 
 fun main() {
     result = test(
         """
-    """.trimIndent()
+             125 17
+     """.trimIndent(), 6
     )
-    check(0, result)
+    check(22, result)
 
     result = test(
         """
-    """.trimIndent()
+            125 17
+     """.trimIndent(), 25
     )
-    check(0, result)
+    check(55312, result)
+
+    result = test(
+        """
+            9759 0 256219 60 1175776 113 6 92833
+    """.trimIndent(), 25
+    )
+    check(186996, result)
+
+    result = test(
+        """
+            9759 0 256219 60 1175776 113 6 92833
+       """.trimIndent(), 75
+    )
+    check(221683913164898, result)
 }
 
-private fun test(input: String): Int {
-    return 0
+private fun test(input: String, count: Int): Long {
+    var numbers = input.split(" ").map(String::toLong).associateWith { 1L }
+    for (i in 1..count) {
+        val newNumbers = mutableMapOf<Long, Long>()
+        for ((number, value) in numbers) {
+            val numberStr = number.toString()
+            when {
+                number == 0L -> {
+                    newNumbers[1] = (newNumbers[1] ?: 0) + value
+                }
+
+                numberStr.length % 2 == 0 -> {
+                    val first = numberStr.substring(0, numberStr.length / 2).toLong()
+                    newNumbers[first] = (newNumbers[first] ?: 0) + value
+                    val second = numberStr.substring(numberStr.length / 2).toLong()
+                    newNumbers[second] = (newNumbers[second] ?: 0) + value
+                }
+
+                else -> {
+                    val newNumber = number * 2024
+                    newNumbers[newNumber] = (newNumbers[newNumber] ?: 0) + value
+                }
+            }
+        }
+        numbers = newNumbers
+    }
+    return numbers.values.sum()
 }
